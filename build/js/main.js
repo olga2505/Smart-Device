3fdf812dae0f4ffbd019c0e498c31d19491f0b9a
Olga <151590@mail.ru>
2021-02-09 16:43:29 +0300

    Исправляет работу аккордеона

--- build/js/main.js
+++ build/js/main.js
@@ -201,20 +201,32 @@
   var titleSection = document.querySelector('.site-sections h2');
   var buttonSections = document.querySelector('.site-sections__button');
   var siteSectionsList = document.querySelector('.site-sections__list');
-
   var titleContacts = document.querySelector('.contacts__wrap h2');
   var buttonContacts = document.querySelector('.contacts__button');
   var contactsBlock = document.querySelector('.contacts__block');
+
+  buttonSections.classList.remove('no-js');
+  buttonSections.classList.add('button-hidden');
+  buttonContacts.classList.remove('no-js');
+  buttonContacts.classList.add('button-hidden');
+
+  siteSectionsList.classList.add('form-hidden');
+  contactsBlock.classList.add('form-hidden');
 
   titleSection.addEventListener('click', function () {
     if (buttonSections.classList.contains('button-hidden')) {
       buttonSections.classList.remove('button-hidden');
       buttonSections.classList.add('button-show');
-      siteSectionsList.classList.toggle('form-show');
+      siteSectionsList.classList.toggle('form-hidden');
+
+      buttonContacts.classList.remove('button-show');
+      buttonContacts.classList.add('button-hidden');
+      contactsBlock.classList.add('form-hidden');
+      contactsBlock.classList.remove('form-show');
     } else {
       buttonSections.classList.add('button-hidden');
       buttonSections.classList.remove('button-show');
-      siteSectionsList.classList.toggle('form-show');
+      siteSectionsList.classList.toggle('form-hidden');
     }
   });
 
@@ -222,11 +234,16 @@
     if (buttonContacts.classList.contains('button-hidden')) {
       buttonContacts.classList.remove('button-hidden');
       buttonContacts.classList.add('button-show');
-      contactsBlock.classList.toggle('form-show');
+      contactsBlock.classList.toggle('form-hidden');
+
+      buttonSections.classList.remove('button-show');
+      buttonSections.classList.add('button-hidden');
+      siteSectionsList.classList.add('form-hidden');
+      siteSectionsList.classList.remove('form-show');
     } else {
       buttonContacts.classList.add('button-hidden');
       buttonContacts.classList.remove('button-show');
-      contactsBlock.classList.toggle('form-show');
+      contactsBlock.classList.toggle('form-hidden');
     }
   });
 })();
