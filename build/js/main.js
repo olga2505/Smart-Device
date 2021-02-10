9e3b74ef3553a8ed40d3af32a5fd74cec28faa4d
Olga <151590@mail.ru>
2021-02-10 20:32:05 +0300

    Исправляет маску

--- build/js/main.js
+++ build/js/main.js
@@ -48,8 +48,7 @@
   var inputTelPopup = document.getElementById('feedback-tel-popup');
   var feedbackTel = document.getElementById('feedback-tel');
   var maskOptions = {
-    mask: '+{7}(000)000-00-00',
-    lazy: false
+    mask: '+{7}(000)000-00-00'
   };
 
   inputTelPopup.addEventListener('focus', function () {
